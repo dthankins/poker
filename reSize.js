924dbaec469b78d7879e4d39cd54db5aa4fb1f15
dthankins <34296958+dthankins@users.noreply.github.com>
2021-09-27 16:28:10 -0500

    changed png to jpg because resized

--- reSize.js
+++ reSize.js
@@ -335,7 +335,7 @@ function setup() {
     }
     
 
-    imageAddressString = 'https://dthankins.github.io/cards/' + the23Cards[i] + '.png';
+    imageAddressString = 'https://dthankins.github.io/cards/' + the23Cards[i] + '.jpg'; // png
     cards[i].cardImage = loadImage(imageAddressString);
 
   }
